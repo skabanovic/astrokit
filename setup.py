7b19c9be85727f70fb98816dc19dc8197b848402
Christof Buchbender <buchbend@ph1.uni-koeln.de>
2020-02-04 10:53:04 +0100

    [refactor] making setup.py installable

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-   name='astrokit',
-   version='1.0',
-   description='toolkit for astro data ',
-   author='Slawa Kabanovic',
-   author_email='kabanovic@ph1.uni-koeln.de',
-   packages=['astrokit'],  #same as name
-   install_requires=['numpy', 'astropy'], #external packages as dependencies
-   scripts=[
-            'astro/celestial.py',
-            'math/curve.py',
-            'math/transform.py',
-            'num/solver.py',
-            'prism/dendrokit.py',
-            'prism/mask.py',
-            'prism/specube.py',
-            'astrokit/watch/progress.py'
-           ]
+    name='astrokit',
+    version='1.0',
+    description='toolkit for astro data ',
+    author='Slawa Kabanovic',
+    author_email='kabanovic@ph1.uni-koeln.de',
+    packages=find_packages(exclude=('doku')),  # same as name
+    # external packages as dependencies
+    install_requires=['numpy', 'astropy', 'astroquery'],
+    # scripts=[
+    #          'astro/celestial.py',
+    #          'math/curve.py',
+    #          'math/transform.py',
+    #          'num/solver.py',
+    #          'prism/dendrokit.py',
+    #          'prism/mask.py',
+    #          'prism/specube.py',
+    #          'astrokit/watch/progress.py'
+    #         ]
+    zip_safe=False
 )
