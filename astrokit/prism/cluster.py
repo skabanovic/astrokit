b1ff235fa6d9e028905d797521f2b9e451095a59
Slawa Kabanovic <slawa@slawas-mbp.speedport.ip>
2020-10-16 17:33:27 +0200

    added weights to gmm function

--- astrokit/prism/cluster.py
+++ astrokit/prism/cluster.py
@@ -4,9 +4,11 @@ from sklearn.cluster import SpectralClustering
 import numpy as np
 import astrokit
 import copy
+import pomegranate
 
 def cluster_information(cube,
                         cluster_range,
+                        weight=None,
                         methode = 'BIC',
                         reduce_dim = 'pca',
                         norm = 'mean',
@@ -14,8 +16,8 @@ def cluster_information(cube,
                         sample = None):
 
     # Dimension of the input cube
-    dim_ax1 = cube[0].header['NAXIS1'] # Points in Y/Dec axis
-    dim_ax2 = cube[0].header['NAXIS2'] # Points in X/RA axis
+    dim_ax1 = cube[0].header['NAXIS1'] # Points in X/RA axis
+    dim_ax2 = cube[0].header['NAXIS2'] # Points in Y/Dec axis
     dim_ax3 = cube[0].header['NAXIS3'] # Points in velocity
 
     # Check: if a grid point is masked with nan at velocity v it should be masked at all v
@@ -27,6 +29,10 @@ def cluster_information(cube,
     # From a [dimV,dimY,dimX] array
     # to a list of (dimY x dimX) spectra of length dimV
     flat_data_masked = cube[0].data.reshape(dim_ax3, dim_ax2*dim_ax1).transpose()
+
+    if weight:
+        flat_weight_masked = weight[0].data.reshape(dim_ax3, dim_ax2*dim_ax1).transpose()
+        flat_weight = flat_weight_masked[~np.isnan(np.sum(flat_data_masked, axis=1))]
 
     # Consider only the points that are not masked
     flat_data = flat_data_masked[~np.isnan(np.sum(flat_data_masked,axis=1))]
@@ -114,9 +120,19 @@ def cluster_information(cube,
 
         sample_data = reduced_data[indices]
 
+        if weight:
+
+            sample_weight = flat_weight[indices]
+
     else:
 
         sample_data = reduced_data
+
+        if weight:
+
+            sample_weight = flat_weight
+
+
 
     info_criterion = []
 
@@ -124,14 +140,46 @@ def cluster_information(cube,
 
     for cluster in cluster_list:
 
-        model = GaussianMixture(n_components = cluster,
-                                covariance_type='full',
-                                max_iter = gmm_iter).fit(sample_data)
+        if weight:
+
+            #Make the GMM model using pomegranate
+            model = pomegranate.gmm.GeneralMixtureModel.from_samples(
+                pomegranate.MultivariateGaussianDistribution,   #Either single function, or list of functions
+                n_components = cluster,     #Required if single function passed as first arg
+                X = sample_data,     #data format: each row is a point-coordinate, each column is a dimension
+                stop_threshold = 0.01,  #Lower this value to get better fit but take longer.
+                max_iterations = 100
+                )
+
+            #Force the model to train again, using additional fitting parameters
+            model.fit(
+                X = sample_data,         #data format: each row is a coordinate, each column is a dimension
+                weights = sample_weight,  #List of weights. One for each point-coordinate
+                stop_threshold = 0.01,  #Lower this value to get better fit but take longer.
+                max_iterations = 100
+                            #   (sklearn likes better/slower fits than pomegrante by default)
+                )
+
+        else:
+
+
+            model = GaussianMixture(n_components = cluster,
+                                    covariance_type ='full',
+                                    max_iter = gmm_iter).fit(sample_data)
+
 
         if methode == 'BIC':
 
-            cluster_info_criterion = model.bic(sample_data)
-            print('cluster = '+str(cluster)+' ---> BIC = '+str(cluster_info_criterion))
+            if weight:
+
+                N_k = cluster-1.+cluster*dim_ax3+(cluster*dim_ax3*(dim_ax3-1.))/2.
+                cluster_info_criterion = N_k*np.log(len(sample_data))-2.*np.sum(model.log_probability(sample_data))
+                print('cluster = '+str(cluster)+' ---> BIC = '+str(cluster_info_criterion))
+
+            else:
+
+                cluster_info_criterion = model.bic(sample_data)
+                print('cluster = '+str(cluster)+' ---> BIC = '+str(cluster_info_criterion))
 
         elif methode == 'AIC':
 
@@ -151,6 +199,7 @@ def duplicates(lst, item):
 
 def spectra_clustering(cube,
                        n_com,
+                       weight = None,
                        gmm_iter = 1000,
                        reduce_dim = 'original',
                        norm = 'mean'):
@@ -171,12 +220,16 @@ def spectra_clustering(cube,
 
             if np.isnan(np.sum(cube[0].data[:,idx_ax2, idx_ax1])):
 
-                cube[0].data[:,idx_ax2,idx_ax1] = np.nan
+                cube[0].data[:, idx_ax2, idx_ax1] = np.nan
 
     # From a [dimV,dimY,dimX] array
     # to a list of (dimY x dimX) spectra of length dimV
     flat_index_masked = index_map.reshape(2, dim_ax2*dim_ax1).transpose()
     flat_data_masked = cube[0].data.reshape(dim_ax3, dim_ax2*dim_ax1).transpose()
+
+    if weight:
+        flat_weight_masked = weight[0].data.reshape(dim_ax3, dim_ax2*dim_ax1).transpose()
+        flat_weight = flat_weight_masked[~np.isnan(np.sum(flat_data_masked, axis=1))]
 
     # Consider only the points that are not masked
     flat_index = flat_index_masked[~np.isnan(np.sum(flat_data_masked, axis=1))]
@@ -252,41 +305,73 @@ def spectra_clustering(cube,
 
         reduced_data = normed_flat_data
 
-    gmm = GaussianMixture(n_components = n_com,
-                          covariance_type='full',
-                          random_state=42,
-                          max_iter = gmm_iter).fit(reduced_data)
+        if weight:
+
+            #Make the GMM model using pomegranate
+            model = pomegranate.gmm.GeneralMixtureModel.from_samples(
+                pomegranate.MultivariateGaussianDistribution,   #Either single function, or list of functions
+                n_components=n_com,     #Required if single function passed as first arg
+                X = reduced_data,     #data format: each row is a point-coordinate, each column is a dimension
+                init = 'first-k',
+                stop_threshold = 0.001,  #Lower this value to get better fit but take longer.
+                max_iterations = gmm_iter
+                )
+
+            #Force the model to train again, using additional fitting parameters
+            model.fit(
+                X=reduced_data,         #data format: each row is a coordinate, each column is a dimension
+                weights = flat_weight,  #List of weights. One for each point-coordinate
+                stop_threshold = 0.001,  #Lower this value to get better fit but take longer.
+                max_iterations = gmm_iter
+                            #   (sklearn likes better/slower fits than pomegrante by default)
+                )
+
+        else:
 
 
-    labels = gmm.predict(reduced_data)
+            model = GaussianMixture(n_components = n_com,
+                                    covariance_type='full',
+                                    random_state=42,
+                                    max_iter = gmm_iter).fit(reduced_data)
 
+
+    labels = model.predict(reduced_data)
+    probs = model.predict_proba(reduced_data)
+
+
+    probs_cube = np.zeros([n_com, dim_ax2, dim_ax1])
     domain_map = astrokit.zeros_map(cube)
+
+    domain_map[0].data[:,:] = np.nan
 
     for label in range(len(labels)):
 
         domain_map[0].data[int(flat_index[label][1]), int(flat_index[label][0])] = labels[label]
+        probs_cube[:, int(flat_index[label][1]), int(flat_index[label][0])] = probs[label, :]
 
-    mode_signal_mean = []
-    mode_signal_std = []
+    #mode_signal_mean = []
+    #mode_signal_std = []
 
     #labels_map = labels.reshape(dim_ax2, dim_ax1).transpose()
 
-    for i in range(n_com):
+    #for i in range(n_com):
 
-        signal = np.mean(cube[0].data[:,domain_map[0].data==i], axis=1)
-        std = np.std(cube[0].data[:, domain_map[0].data==i], axis=1)
+    #    signal = np.mean(cube[0].data[:,domain_map[0].data==i], axis=1)
+#        std = np.std(cube[0].data[:, domain_map[0].data==i], axis=1)
 
-        mode_signal_mean.append(signal)
-        mode_signal_std.append(std)
+    #    mode_signal_mean.append(signal)
+    #    mode_signal_std.append(std)
 
-    return mode_signal_mean, mode_signal_std, domain_map
+    #return mode_signal_mean, mode_signal_std, domain_map, probs_cube
+    return domain_map, probs_cube
+
 
 
 def cluster_average(cube,
                     cluster_map,
                     weight_map = None):
 
-    cluster_num = int(np.max(cluster_map[0].data)) +1
+    cluster_num = int(np.nanmax(cluster_map[0].data)) +1
     len_ax3 = cube[0].header['NAXIS3']
     cluster_spect = np.zeros([cluster_num, len_ax3])
 
@@ -301,5 +386,7 @@ def cluster_average(cube,
 
             cluster_spect[cluster, :] = np.average(cube[0].data[:, cluster_map[0].data == cluster], axis = 1)
 
+        cluster_std = np.std(cube[0].data[:, cluster_map[0].data == cluster], axis = 1)
 
-    return cluster_spect
+
+    return cluster_spect, cluster_std
